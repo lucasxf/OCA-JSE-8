cb0bffcbd301c2417e5a3c1f98f2265cb3244f8e
lucasxf <lucasxferreira@gmail.com>
2018-01-15 20:56:30 -0200

    Finished self-test question 2. Got the right option

--- book/chapter5/self-test/q2/Plane.java
+++ book/chapter5/self-test/q2/Plane.java
@@ -1,8 +1,27 @@
 // Chapter 5, Self-test Question 2
-// My answer: ) "-rg"
-// Actual answer: c) "-rg"
+// My answer: h) Compilation fails. (void s2 without try/throw)
+// Actual answer: h) Compilation fails.
 class Plane {
 
     static String s = "-";
+
+    public static void main (String[] args) {
+        new Plane().s1();
+        System.out.println(s);
+    }
+
+    void s1() {
+        try { s2(); }
+        catch( Exception e) { s += "c"; }
+    }
+
+    void s2() {
+        s3(); s += "2";
+        s3(); s += "2b";
+    }
+
+    void s3() throws Exception {
+        throw new Exception();
+    }
 
 }
