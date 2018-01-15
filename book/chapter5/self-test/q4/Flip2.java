ea4e2b4c4563b7fddb7a8cbde36a606ddcac8a10
lucasxf <lucasxferreira@gmail.com>
2018-01-15 21:14:11 -0200

    Finished q4, got it right

--- book/chapter5/self-test/q4/Flip2.java
+++ book/chapter5/self-test/q4/Flip2.java
@@ -1,4 +1,8 @@
 // Input: RED Green YeLLow
+// Chapter 5, Self-test Question 4
+// My answer: e) An exception is thrown at runtime.
+// (sa.len > args.len, n will eventually throw a NullPointerException)
+// Actual answer: e) An exception is thrown at runtime.
 public class Flip2 {
 
     public static void main (String[] args) {
@@ -6,9 +10,11 @@ public class Flip2 {
         String o = "-";
 
         String[] sa = new String[4];
+
         for ( int i = 0; i < args.length; i++ ) {
             sa[i] = args[i];
         }
+
         for ( String n : sa ) {
             switch( n.toLowerCase() ) {
                 case "yellow": o += "y";
