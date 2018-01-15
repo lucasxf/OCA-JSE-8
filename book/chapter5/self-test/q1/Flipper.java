e362afd5da62119e2651822a09913b768e4ae42a
lucasxf <lucasxferreira@gmail.com>
2018-01-15 20:41:28 -0200

    Runned question (chosen the right answer)

--- book/chapter5/self-test/q1/Flipper.java
+++ book/chapter5/self-test/q1/Flipper.java
@@ -1,9 +1,12 @@
+// Chapter 5, Self-test Question 1
+// My answer: c) "-rg"
+// Actual answer: c) "-rg"
 public class Flipper {
 
     public static void main (String[] args) {
 
         String o = "-";
-        
+
         switch ("RED".toLowerCase()) {
             case "yellow":
                 o += "y";
