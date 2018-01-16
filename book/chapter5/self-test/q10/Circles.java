f7a30dd296189d88d2169b6d1bbf60576fc8e763
lucasxf <lucasxferreira@gmail.com>
2018-01-15 22:26:59 -0200

    Finished C5 ST Q10. Correct answer

---
+++ book/chapter5/self-test/q10/Circles.java
@@ -0,0 +1,19 @@
+// Chapter 5, Self-test Question 10
+// My answer: D)"1 1 3 3 9 9".
+// Actual answer: D) "1 1 3 3 9 9"
+
+public class Circles {
+    public static void main ( String[] args) {
+        int[] ia = { 1, 3, 5, 7, 9};
+
+        for ( int x : ia) {
+            for ( int j = 0; j < 3; j++ ) {
+                if ( x > 4 && x < 8) continue;
+                System.out.println(" " + x);
+                if ( j == 1) break;
+                continue;
+            }
+            continue;
+        }
+    }
+}
